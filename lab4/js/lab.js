ec66862151d7650324901c7f47e181f084dd0ced
didigurto <dagurto@ucsc.edu>
2022-04-20 13:16:46 -0700

    Lab 5_01
    lab 5 assignment

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+/**
+ * 2D Platformer
+ *
+ * Pseudocode for creating a 2D Platforming game
+ *
+ * @link   https://didigurto.github.io/art101/lab4/index.html
+ * @file   This files defines the MyClass class.
+ * @author Diego Agurto
+ */
+
+//1.Generating 2d environment
+//2. Creating main character
+//3. Establishing user input
+//4. Validating user input
+//5. Setting point system
+//6. Establishing win-state
---
+++ lab5/js/lab.js
@@ -0,0 +1,15 @@
+//variables
+make = "Honda";
+model = "Civic";
+color = "Grey";
+var year = 2019;
+ownIt = true;
+var age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Do I own it?: " + ownIt + "<br>");
+document.writeln("How old is it?: " + age + "<br>");

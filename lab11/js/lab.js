27da91ddaa40a2a8f297716af83267b0c46597f3
didigurto <dagurto@ucsc.edu>
2022-05-16 12:49:42 -0700

    Lab 11
    Lab 11 assignment

---
+++ lab11/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Diego
+ * Created:   05.011.2022
+ *
+ **/
+
+$("#butt1").click(function(){
+	$("#text1").toggleClass("selected1");
+})
+
+$("#butt2").click(function(){
+	$("#text2").toggleClass("selected2");
+})
+
+$("#butt3").click(function(){
+	$("#text3").toggleClass("selected3");
+})
+
+$("#butt4").click(function(){
+	$("#butt5").toggleClass("selected5");
+})
